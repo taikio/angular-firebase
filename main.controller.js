1a084da8a5644c2438b90cb2ef3a69af12cc3ec1
Welker Arantes <welker.arantes@timesharesolucoes.com.br>
2018-08-17 18:00:07 -0300

    adicionada a opção de excluir

--- main.controller.js
+++ main.controller.js
@@ -51,6 +51,10 @@
 
                     });
             }
+
+            vm.excluir = function (element){
+                vm.firebasedatabase.$remove(element);
+            }
             
         }
 })();
